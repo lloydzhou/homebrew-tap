07731bb16b71c520c3c99a7269cfc65eb2135eba
lloydzhou <lloydzhou@qq.com>
2026-07-11 15:59:04 +0800

    bash-guard 0.1.3

--- Formula/bash-guard.rb
+++ Formula/bash-guard.rb
@@ -1,25 +1,23 @@
 class BashGuard < Formula
   desc "Claude Code Bash Hook 的失败关闭权限检查器"
   homepage "https://github.com/lloydzhou/claude-bash-guard"
-  version "0.1.2"
-
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v#{version}/bash-guard-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "af69c7bb30160f5dbe8fcfe0716be6c025032903d6c3c6e489ce5fc98907b625"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "d5ce3c6d0d33f46c0f3a47f81f3fb42d74452f9962f7241c9efa6bd5e9623f7e"
     else
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v#{version}/bash-guard-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "e5bbd3a18e04c4a7b04edf04e6eceace6eeec4240520981a3c77e67a3632db37"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "9d2f1e12b2d9426fc487ece495ea6432e087688864ff8fb1cdc74f86fd2e411c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v#{version}/bash-guard-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "55bf95be5221bda43e87fffb0d31735282ee8d01ff9fd33dec3e53f447b88172"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "87c353b68df2e763543a5c17bd90726a2c26cc00e1e403865210689434470940"
     else
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v#{version}/bash-guard-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "35623f62c89b6f90a7174e96be5339dc6710ac32772f6f893583c3d28e00e32b"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e12ad1e342979d515080d422427feb07109df91f3fdeb95be0aaf35fd76a0705"
     end
   end
 
@@ -28,6 +26,6 @@ class BashGuard < Formula
   end
 
   test do
-    assert_match "用法", shell_output("#{bin}/bash-guard", 2)
+    assert_match "用法", shell_output(bin/"bash-guard", 2)
   end
 end
