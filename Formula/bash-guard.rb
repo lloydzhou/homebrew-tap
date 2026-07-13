3b62862679291762436d72524b4c8001fea17f97
lloydzhou <lloydzhou@qq.com>
2026-07-13 15:03:03 +0800

    bash-guard 0.1.4

--- Formula/bash-guard.rb
+++ Formula/bash-guard.rb
@@ -3,21 +3,21 @@ class BashGuard < Formula
   homepage "https://github.com/lloydzhou/claude-bash-guard"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-aarch64-apple-darwin.tar.gz"
-      sha256 "d5ce3c6d0d33f46c0f3a47f81f3fb42d74452f9962f7241c9efa6bd5e9623f7e"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.4/bash-guard-0.1.4-aarch64-apple-darwin.tar.gz"
+      sha256 "f07773e1abdbc6f571f3b818685eea5577145bf974ded30553aadd7f547d8e7a"
     else
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-x86_64-apple-darwin.tar.gz"
-      sha256 "9d2f1e12b2d9426fc487ece495ea6432e087688864ff8fb1cdc74f86fd2e411c"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.4/bash-guard-0.1.4-x86_64-apple-darwin.tar.gz"
+      sha256 "eb06faf31d48f9c5076a2694046041fa18bfa22fab32dd24c7bdbd39f4d834e9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "87c353b68df2e763543a5c17bd90726a2c26cc00e1e403865210689434470940"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.4/bash-guard-0.1.4-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3178a0a09f199a47c7024d3e4efa3d65749211e6fe4917a401fc2ae63d7c02bb"
     else
-      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.3/bash-guard-0.1.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e12ad1e342979d515080d422427feb07109df91f3fdeb95be0aaf35fd76a0705"
+      url "https://github.com/lloydzhou/claude-bash-guard/releases/download/v0.1.4/bash-guard-0.1.4-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "fc2edadd55703b46a71fc8e802844924c96544d2b7a25e5eab30b502ae12d9af"
     end
   end
 
